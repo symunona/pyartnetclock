7c74795aabed450dadc20ac61bacf299aa511efd
Bendeguz <symunona@gmail.com>
2020-12-05 16:10:32 +0100

    countdown mode

--- clock.py
+++ clock.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 from StupidArtnet import StupidArtnet
-import time,os,sys
+import time,os,sys,select
+
+import curses, time
+
+
 
 # art net clock
 # each digit has to be wired the same
@@ -10,6 +14,12 @@ import time,os,sys
 # just don't be in any shock!
 
 # staring address: 0 is default
+
+reverse = False
+
+countdown = True
+
+times = ['05:00:00', '20:00:00']
 
 dmx_start_address = 0
 
@@ -23,7 +33,6 @@ universe = 0 					# see docs
 # each character is 7 bytes, and we have 6 characters, thats 42
 
 packet_size = 42				# it is not necessary to send whole universe
-
 
 # indexes are marked as follows, starting from 0:
 # hour: 0 7  minutes: 14 21
@@ -66,6 +75,9 @@ def send_sequence(sequence):
 
     packet = bytearray()		# create packet for Artnet
 
+    if reverse:
+        sequence = sequence[::-1]
+
     for char in sequence:
         if (char == '0'):
             packet.append(0x00)
@@ -77,9 +89,19 @@ def send_sequence(sequence):
 
 def main(argv):
     global numbers
+    global countdown
 
     while True:
-        time.sleep(refresh_time)
+
+        input = select.select([sys.stdin], [], [], 1)[0]
+        if input:
+            value = sys.stdin.readline().rstrip()
+            if (value == "q"):
+                print("Exiting")
+                sys.exit(0)
+            else:
+                countdown = not(countdown)
+
         # os.system("clear")
 
         h = time.localtime().tm_hour
@@ -87,11 +109,35 @@ def main(argv):
         s = time.localtime().tm_sec
         time_str = f'{h:02}{m:02}{s:02}'
 
+
+        if countdown:
+            if time_str > times[-1]:
+                current_count_down = times[0]
+            else:
+                i = 0
+                while (times[i] < time_str):
+                    i = i + 1
+                current_count_down = times[i]
+
+
+            h_dest = int(current_count_down[0:2])
+            m_dest = int(current_count_down[3:5])
+            s_dest = int(current_count_down[6:8])
+
+            h = h_dest - h
+            m = (60 + m_dest - m) % 60
+            s = (60 + s_dest - s) % 60
+
+            time_str = f'{h:02}{m:02}{s:02}'
+
         sequence = ''
+
+        # print(current_count_down)
+        # print(time_str)
+
         for char in time_str:
             sequence = sequence + numbers[int(char)]
 
-        # print(time_str + '\033[1;31m')
         print(sequence + '\033[1;31m')
         send_sequence(sequence)
         # print('\033[0m')
